09214261abd24a6611cc2eb86557a88db0c5f70f
anizadikian <ani.zadikian@edu.aua.am>
2019-10-27 20:42:18 +0400

    Final Code

--- BrowserHW.py
+++ BrowserHW.py
@@ -1,40 +1,116 @@
+class BrowserHistory:
+
+    def __init__(self, url=None):
+        self.url = url
+        self.next = None
+
 class Stack:
+
     def __init__(self):
-        self.stack = []
-    def pop(self):
-        if self.is_empty():
+        self.__head = None
+
+    def push(self, newurl):
+        if self.__head == None:
+            self.__head = BrowserHistory(newurl)
+
+        else:
+            newData = BrowserHistory(newurl)
+            newData.next = self.__head
+            self.__head = newData
+
+    def back(self):
+        if self.isEmpty():
             return None
         else:
-            return self.stack.pop()
-    def push(self,val):
-        return self.stack.append(val)
-    def peak(self):
-        if self.is_empty():
+            poppedData = self.__head
+            self.__head = self.__head.next
+            global frw
+            frw = poppedData
+            poppedData.next = None
+            return poppedData.url
+
+    def open(self):
+        if self.isEmpty():
             return None
         else:
-            return self.stack[-1]
-    def size(self):
-        return len(self.stack)
-    def is_empty(self):
-        return self.size() == 0
+            return self.__head.url
+
+    def display(self):
+        printval = self.__head
+        if self.isEmpty():
+            print("Stack is empty")
+        else:
+            while printval is not None:
+                print(printval.url)
+                printval = printval.next
+
+
+    def forward(self):
+        temp = self.__head
+        if temp.next is not None:
+            print(frw.url)
+        else:
+            print("No Data")
+
+    def isEmpty(self):
+        if self.__head == None:
+            return True
+        else:
+            return False
+
 
 def main():
 
+    browserhistory = Stack()
+
     while True:
+
         print("Please choose number from the following:")
         print("1 : Insert new URL")
         print("2 : View Browser History")
-        print("3 : Exit the application")
+        print("3 : Edit Browser History")
+        print("4 : Exit the application")
 
         user_input = int(input())
 
         if user_input == 1:
-            newURL = input("Please insert new URL")
+            while True:
+                url = input("Insert the url, such as www.hotmail.com:  ")
+                browserhistory.push(url)
+                break
 
-        if user_input == 2:
-                print("All browser history:")
-                while True:
-                    print("Please choose number from the following:")
-                    print("1 : Go back in browser history")
-                    print("2 : Go forward in browser history")
+        elif user_input == 2:
+            print("All browser history:")
+            browserhistory.display()
 
+
+        elif user_input == 3:
+
+            browserhistory.display()
+
+            while True:
+                print("Please choose number from the following:")
+                print("1 : Go back in browser history")
+                print("2 : Go forward in browser history")
+
+                user_input2 = int(input())
+                if user_input2 == 1:
+                    browserhistory.back()
+                    browserhistory.display()
+                    break
+                elif user_input2 == 2:
+                    browserhistory.forward()
+                    browserhistory.display()
+                    break
+                else:
+                    print("Please type as required, inserting the number 1 or 2")
+
+
+        elif user_input == 4:
+            exit()
+
+        else:
+
+            print("Please type as required, inserting the number 1, 2, 3 or 4")
+
+main()
